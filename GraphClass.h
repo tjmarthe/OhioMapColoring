5f396d4cb52fefcc60aac31f0a7ac38e6d2a30ba
tjmarthe <31713785+tjmarthe@users.noreply.github.com>
2018-11-02 17:31:15 -0400

    Update GraphClass.h

--- GraphClass.h
+++ GraphClass.h
@@ -3,23 +3,24 @@
 //if the boarder arrays for the nodes are enough to work with.
 
 
-#include "NodeClass.h"
-
+#include "CountyNode.h"
+#include "CountyClusterClass.h"
 
 public class OhioGraph
 {
 public:
-	OhioGraph(): NumberOfCounties_(0) , countyMap_[0][0] {};
+	OhioGraph(): NumberOfNodes_(0) , countyMap_[0][0] {};
 
-	OhioGraph(int NumberOfCounties, int countyMap[NumberOfCounties][NumberOfCounties]):
-		NumberOfCounties_(NumberOfCounties) , countyMap_[countyMap] {}; 
-		
+	OhioGraph(int NumberOfNodes):
+		NumberOfNodes_(NumberOfNodes) , countyMap_[NumberOfNodes][NumberOfNodes] {}; 
+	
+
 
 private:
 
-	int NumberOfCounties_;
+	int NumberOfNodes_; //Nodes being regions, counties, etc.
 
-	int countyMap_[NumberOfCounties_][NumberOfCounties_];//Matrix to represent boarders and the map as a whole
+	int countyMap_[NumberOfNodes_][NumberOfNodes_];//Matrix to represent boarders and the map as a whole
 														//Node Connections can represented as 1 or 0 if they are 
 														//Connected or not, 1 = edge connection between node pair
 														//0 = no edge connection between node pair.	
