e0f55f9ecb0a315d88400a16891f87a943c8a6bd
tjmarthe <31713785+tjmarthe@users.noreply.github.com>
2018-11-21 14:26:22 -0500

    Update CountyNode.h

--- CountyNode.h
+++ CountyNode.h
@@ -1,47 +1,44 @@
 //Node class for graph, Nodes represent vertices of connected edges.
 //Edges will represent boarders of Ohio counties.
 //Nodes will represent the actual counties
-//Nodes will need an array for boardering states, 
+//Nodes will need an array for boardering states,
 //with pointers to the information on the other nodes.
 //Nodes also have a color value
-
-#include <cstdlib>
-#include <iostream>
 #include <string.h>
-
-public class CountyNode
+#include <iostream>
+class CountyNode
 {
 
-public:
-	
-	CountyNode(): countyNumber_(0), boarderingCounties_(0) , colorValue_(0) , boarderArray_(0) {}
+ public:
 
-	CountyNode(int countyNumber, int boarderingCounties, int colorValue): 
-		countyNumber(0), boarderingCounties_(boarderingCounties) , colorValue_(colorValue) 
-		, boarderArray_ = CountyNode new[boarderingCounties_] {};
+ CountyNode(): countyNumber_(0), boarderingCounties_(0) , colorValue_(0) , boarderArray_(0) {}
 
-	int getCountyNumber() return countyNumber_;{}
+ CountyNode(int countyNumber, int boarderingCounties, int colorValue):
+  countyNumber_(countyNumber), boarderingCounties_(boarderingCounties) , colorValue_(colorValue) {};
 
-	int getBoarderingCounties() return boarderingCounties_;{}
+  int getCountyNumber() {return countyNumber_;}
 
-	int getColor() return colorValue_; {}
+  int getBoarderingCounties() {return boarderingCounties_;}
 
-	CountyNode getBoarders() return boarderArray_;{}
+  int getColor() {return colorValue_;}
 
-	void printCounty(){
-		cout << name_ << endl;
-		}
+  CountyNode getBoarders() {return boarderArray_;}
 
-private:
-	
-	string name_;
+  void printCounty(){
+    std::cout << name_ << std::endl;
+  }
 
-	int countyNumber_;
+ private:
 
-	int boarderingCounties_;
+  std::string name_;
 
-	int colorValue_; //Made this an int instead of a string because 
-					//I think it'll be easier to compare this way.
+  int countyNumber_;
 
-	CountyNode* boarderArray_ = CountyNode new[boarderingCounties_];
+  int boarderingCounties_;
+
+  int colorValue_; //Made this an int instead of a string because
+  //I think it'll be easier to compare this way.
+
+  CountyNode* boarderArray_ = CountyNode new[boarderingCounties_];
 };
+
