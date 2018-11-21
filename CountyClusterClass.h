7bb046db4212052daf36ab572c2ea0872f561727
tjmarthe <31713785+tjmarthe@users.noreply.github.com>
2018-11-20 19:09:47 -0500

    Update and rename CounterClusterClass.h to CountyClusterClass.h

--- CountyClusterClass.h
+++ CountyClusterClass.h
@@ -7,19 +7,19 @@
 #include "GraphClass.h"
 #include "CountyNode.h"
 
-public class CounterCluster
+public class CountyCluster
 {
 public:
 
-	CounterCluster(int clusterNumber, int clusterColor, int suroundingClusters, int counties): 
+	CountyCluster(int clusterNumber, int clusterColor, int suroundingClusters, int counties): 
 		clusterNumber_(clusterNumber) , clusterColor(clusterColor_), surroundingClusters(suroundingClusters_)	
 		, surroundingClustersArray_ = CounterCluster new[surroundingClusters], int counties_(counties), 
-			countyArray_ = CountyNode new[counties] {};	
+		countyArray_ = CountyNode new[counties] {};	
 	
-	CounterCluster(): 
+	CountyCluster(): 
 		clusterNumber_(0), clusterColor_(0), surroundingClusters_(0), surroundingClustersArray_(0) {};
 	
-	~CounterClusterClass(){
+	~CountyClusterClass(){
 		delete[] surroundingClusters_;
 		delete[] countyArray_;
 		}
@@ -38,16 +38,32 @@ public:
 		return countyArray_;
 	}
 
+	CountyClusterClass& operator=(CountyClusterClass rhs){
+		int clusterColor_ = rhs.clusterColor_;
+		int counties_ = rhs.counties_;
+		int clusterNumber_ = rhs.clusterNumber_;
+		int surroundingClusters_ = rhs.surroundingClusters_;
+		for(int i = 0; i < surroundingClusters_; ++i){
+			surroundingClustersArray_[i] = rhs.surroundingClustersArray_[i];
+		}
+		for(int i = 0; i < counties_; ++i){
+			countyArray_ = rhs.countyArray_;
+		}
+
+		return *this;
+	}
+
+
 	void printCluster(){
 		for (int i = 0; i < counties_; ++i)
 			cout << countyArray_[i] << endl;
 		}
 private:
 	int clusterNumber_;
-	GraphClass countyGraph_;
+	GraphClass countyGraph_; //To keep track of boarder connections(edge connections)
 	int counties_;
 	int clusterColor_;
 	int surroundingClusters_;
-	CountyCluster* surroundingClustersArray_ = CounterCluster new[surroundingClusters_];
-	CountyNode* countyArray_ = CountyNode new[counties];
+	CountyCluster* surroundingClustersArray_ = CountyCluster new[surroundingClusters_]; //Number of contraints = surrounding clusters
+	CountyNode* countyArray_ = CountyNode new[counties]; //Keep track of conties in the graph.
 };
