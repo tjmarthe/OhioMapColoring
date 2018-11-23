dde2e26ae3b0f27d162a25a1054f46f0c5801465
tjmarthe <31713785+tjmarthe@users.noreply.github.com>
2018-11-23 14:37:45 -0500

    Update MapColor.cpp

--- MapColor.cpp
+++ MapColor.cpp
@@ -37,94 +37,93 @@ void color(CountyClusterClass curr){
 
 int main(){
 
-	CountyClusterClass one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve;
-	
-	//These are each node, with an array of each touching node
-	one.surroundingClusters_ = 4;
-	one.surroundingClusterArray_[0] = two;
-	one.surroundingClusterArray_[1] = three;
-	one.surroundingClusterArray_[2] = six;
-	one.surroundingClusterArray_[3] = seven;
+  CountyCluster one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve;
 
-	two.surroundingClusters_ = 2;
-	two.surroundingClusterArray_[0] = one;
-	two.surroundingClusterArray_[1] = three;
+  //These are each node, with an array of each touching node
+  one.getsuroundingClusters(4);
+  one.giveConnection(0, two);
+  one.giveConnection(1, three);
+  one.giveConnection(2, six);
+  one.giveConnection(3, seven);
 
-	three.surroundingClusters_ = 7;
-	three.surroundingClusterArray_[0] = two;
-	three.surroundingClusterArray_[1] = five;
-	three.surroundingClusterArray_[2] = one;
-	three.surroundingClusterArray_[3] = six;
-	three.surroundingClusterArray_[4] = eleven;
-	three.surroundingClusterArray_[5] = four;
-	three.surroundingClusterArray_[6] = twelve;
+  two.getsuroundingClusters(2);
+  two.giveConnection(0, one);
+  two.giveConnection(1, three);
 
-	four.surroundingClusters_ = 3;
-	four.surroundingClusterArray_[0] = three;
-	four.surroundingClusterArray_[1] = eleven;
-	four.surroundingClusterArray_[2] = twelve;
+  three.getsuroundingClusters(7);
+  three.giveConnection(0, two);
+  three.giveConnection(1, five);
+  three.giveConnection(2, one);
+  three.giveConnection(3, six);
+  three.giveConnection(4, eleven);
+  three.giveConnection(5, four);
+  three.giveConnection(6, twelve);
 
-	five.surroundingClusters_ = 4;
-	five.surroundingClusterArray_[0] = three;
-	five.surroundingClusterArray_[1] = six;
-	five.surroundingClusterArray_[3] = eleven
-	five.surroundingClusterArray_[4] = ten;
+  four.getsuroundingClusters(3);
+  four.giveConnection(0, three);
+  four.giveConnection(1, eleven);
+  four.giveConnection(2, twelve);
 
-	six.surroundingClusters_ = 7;
-	six.surroundingClusterArray_[0] = three;
-	six.surroundingClusterArray_[1] = five;
-	six.surroundingClusterArray_[2] = one;
-	six.surroundingClusterArray_[3] = nine;
-	six.surroundingClusterArray_[4] = seven;
-	six.surroundingClusterArray_[5] = ten;
-	six.surroundingClusterArray_[6] = eight;
+  five.getsuroundingClusters(4);
+  five.giveConnection(0, three);
+   five.giveConnection(1, six);
+  five.giveConnection(3, eleven);
+  five.giveConnection(4, twelve);
 
-	seven.surroundingClusters_ = 3;
-	seven.surroundingClusterArray_[0] = six;
-	seven.surroundingClusterArray_[1] = eight;
-	seven.surroundingClusterArray_[2] = one;
+  six.getsuroundingClusters(7);
+  six.giveConnection(0, three);
+  six.giveConnection(1, five);;
+  six.giveConnection(2, one);;
+  six.giveConnection(3, nine);
+  six.giveConnection(4, seven);
+ six.giveConnection(5, ten);
+ six.giveConnection(6, eight);
 
-	eight.surroundingClusters_ = 3;
-	eight.surroundingClusterArray_[0] = seven;
-	eight.surroundingClusterArray_[1] = nine;
-	eight.surroundingClusterArray_[2] = six;
+  seven.getsuroundingClusters(3);
+  seven.giveConnection(0, six);
+  seven.giveConnection(1, eight);
+  seven.giveConnection(2, one);
 
-	nine.surroundingClusters_ = 3
-	nine.surroundingClusterArray_[0] = eight;
-	nine.surroundingClusterArray_[1] = six;
-	nine.surroundingClusterArray_[2] = ten;
+  eight.getsuroundingClusters(3);
+  eight.giveConnection(0, seven);
+  eight.giveConnection(1, nine);
+  eight.giveConnection(2, six);
 
-	ten.surroundingClusters_ = 3
-	ten.surroundingClusterArray_[0] = five;
-	ten.surroundingClusterArray_[1] = eleven;
-	ten.surroundingClusterArray_[2] = nine;
+  nine.getsuroundingClusters(3);
+  nine.giveConnection(0, eight);
+  nine.giveConnection(1, six);
+  nine.giveConnection(2, ten);
 
-	eleven.surroundingClusters_ = 4
-	eleven.surroundingClusterArray_[0] = ten;
-	eleven.surroundingClusterArray_[1] = five;
-	eleven.surroundingClusterArray_[2] = three;
-	eleven.surroundingClusterArray_[3] = four;
+  ten.getsuroundingClusters(3);
+  ten.giveConnection(0, five);
+  ten.giveConnection(1, eleven);
+  ten.giveConnection(2, nine);
 
-	twelve.surroundingClusters_ = 2
-	twelve.surroundingClusterArray_[0] = three;
-	twelve.surroundingClusterArray_[1] = four;
+  eleven.getsuroundingClusters(4);
+  eleven.giveConnection(0, ten);
+  eleven.giveConnection(1, five);
+  eleven.giveConnection(2, three);
+  eleven.giveConnection(3, four);
 
-	CountyClusterClass ohioArray[12];
+  twelve.getsuroundingClusters(2);
+  twelve.giveConnection(0, three);
+  twelve.giveConnection(1, four);
 
-	//each node
-	ohioArray[0] = one;
-	ohioArray[1] = two;
-	ohioArray[2] = three;
-	ohioArray[3] = four;
-	ohioArray[4] = five;
-	ohioArray[5] = six;
-	ohioArray[6] = seven;
-	ohioArray[7] = eight;
-	ohioArray[8] = nine;
-	ohioArray[9] = ten;
-	ohioArray[10] = eleven;
-	ohioArray[11] = twelve;
+  CountyCluster ohioArray[12];
 
+  //each node
+  ohioArray[0] = one;
+  ohioArray[1] = two;
+  ohioArray[2] = three;
+  ohioArray[3] = four;
+  ohioArray[4] = five;
+  ohioArray[5] = six;
+  ohioArray[6] = seven;
+  ohioArray[7] = eight;
+  ohioArray[8] = nine;
+  ohioArray[9] = ten;
+  ohioArray[10] = eleven;
+  ohioArray[11] = twelve;
 
 	int nodeLength[11] = { 10 , 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
 	int tempLength;
