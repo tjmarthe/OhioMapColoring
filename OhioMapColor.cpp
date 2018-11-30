b743b121a9f59ac0eefeb4c44b0b44754427fd0d
tjmarthe <31713785+tjmarthe@users.noreply.github.com>
2018-11-30 14:51:12 -0500

    Update OhioMapColor.cpp

--- OhioMapColor.cpp
+++ OhioMapColor.cpp
@@ -1,94 +1,117 @@
 //Program to color graphs based on their edges/vertex relationship
-
-
-
 #include <iostream>
 #include <list>
+#include <string>
+
+using namespace std;
 
 
 
 class ColorGraph{
 public:
-  ColorGraph(int num){
-    NodeNum = num;
+  ColorGraph(int num)
+  {
+  	NodeNum = num;
     BoarderList = new std::list<int>[num];
   }
-  ~ColorGraph(){
+  ~ColorGraph()
+  {
     delete [] BoarderList;
   }
 
-  void Color(){
-
+  void Color()
+  {
     int colors[NodeNum];
+    string actualColors[] = {"red", "green", "blue", "yellow"};
     colors[0]  = 0;
 
-   for (int i = 1; i < NodeNum; ++i)
-      colors[i] = -1;
+    for (int i = 1; i < NodeNum; ++i)
+    	colors[i] = -1;
 
 
     bool possible[NodeNum];
     for (int x = 0; x < NodeNum; ++x)
-      possible[x] = false;
+    	possible[x] = false;
 
 
     for (int u = 1; u < NodeNum; ++u)
-      {
-        std::list<int>::iterator IT;
+    {
+    	list<int>::iterator IT;
         for (IT = BoarderList[u].begin(); IT != BoarderList[u].end(); ++IT)
-          if (colors[*IT] != -1)
+        	if (colors[*IT] != -1)
             possible[colors[*IT]] = true;
         int chosenColor;
         for (chosenColor = 0; chosenColor < NodeNum; chosenColor++)
-          if (possible[chosenColor] == false)
-            break;
+        	if (possible[chosenColor] == false)
+        		break;
         colors[u] = chosenColor;
         for (IT = BoarderList[u].begin(); IT != BoarderList[u].end(); ++IT)
-          if (colors[*IT] != -1) {
-            possible[colors[*IT]] = false;
-          }
-      }
-    for (int j = 0; j < NodeNum; j++)
-      std::cout << "Node number " << j << " has the color of: " << colors[j] << std::endl;
-  }
+        	if (colors[*IT] != -1)
+			{
+        		possible[colors[*IT]] = false;
+        	}
+    }
+    
+	for (int j = 0; j < NodeNum; j++)
+	{
+    	cout << "Node number " << j << " has the color of: ";
+    	switch(colors[j])
+    	{
+    		case 0 : cout << actualColors[0];
+    			break;
+    		case 1 : cout << actualColors[1];
+    			break;
+    		case 2 : cout << actualColors[2];
+    			break;
+    		case 3 : cout << actualColors[3];
+    			break;
+    		default: cout << "color not assigned.";
+    			break;
+		}
+		cout << endl;
+	}
+}
 
- void giveConnection(int i, int j){
-   BoarderList[j].push_back(i);
-   BoarderList[i].push_back(j);
- }
+void giveConnection(int i, int j)
+{
+	BoarderList[j].push_back(i);
+    BoarderList[i].push_back(j);
+}
 private:
 
-  int NodeNum;
-  std::list<int> *BoarderList;
+	int NodeNum;
+  	list<int> *BoarderList;
 };
 
 
 int main(){
 
+
   ColorGraph OhioGraph(12);
 
-/* connection for ohio graph */
+  /* connections for ohio grpah */
+  
 
-
-  std::cout << "How many nodes in your map to color? : ";
+  cout << "How many nodes in your map to color? : ";
   int nodes;
-  std::cin >> nodes;
+  cin >> nodes;
 
   ColorGraph graph(nodes);
 
-  std::cout << "Please enter your connections." << std::endl;
+  cout << "Please enter your connections." << std::endl;
   int connector;
   int connectie;
-  std::string toEnd;
+  string toEnd;
   while(toEnd != "yes"){
-    std::cout << "Node: ";
-    std::cin >> connector;
-    std::cout << "is connected to Node: ";
-    std::cin >> connectie;
+    cout << "Node: ";
+    cin >> connector;
+    cout << "is connected to Node: ";
+    cin >> connectie;
 
     graph.giveConnection(connector, connectie);
 
-    std::cout << "Are you finished with your connections? (enter 'yes' to finish)" << std::endl;
-    std::cin >> toEnd;
+    cout << "Are you finished with your connections? (enter 'yes' to finish)" << std::endl;
+    cin >> toEnd;
   }
   graph.Color();
 }
